6623edbdb2075160fb8ce58cd0343fc389afa21d
Clwmm <emgie4432@gmail.com>
2024-11-04 20:29:56 +0100

    init

---
+++ Readme.md
@@ -0,0 +1,3 @@
+1. Run ```source ../../.virtualenvs/GUI/bin/activate```
+2. Run ```uvicorn backend.main:app --reload```
+3. Run ```npx tailwindcss -i ./frontend/static/css/input.css -o ./frontend/static/css/output.css --watch```
---
+++ backend/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from backend.auth import auth
+import os
+from dotenv import load_dotenv, find_dotenv
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+load_dotenv(find_dotenv())
+
+origins = ["http://localhost:20002", "http://127.0.0.1:20002"]
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="add any string...")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+app.include_router(auth.router, tags=["auth"])
+
+templates = Jinja2Templates(directory="frontend/templates")
+
+
+@app.get("/")
+def index(request: Request):
+    user = request.session.get("user")
+    if user:
+        return RedirectResponse("hi")
+
+    return templates.TemplateResponse(name="home.html", context={"request": request})
