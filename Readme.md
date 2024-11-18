c048cf6f367e8c86138882df0eace0edf474241c
Clwmm <emgie4432@gmail.com>
2024-11-19 00:52:01 +0100

    added lightweight-charts with simple chart

--- Readme.md
+++ Readme.md
@@ -10,6 +10,11 @@ GOOGLE_CLIENT_ID=<google client id api key console.cloud.google.com>
 GOOGLE_CLIENT_SECRET=<google client secret api key console.cloud.google.com>
 ```
 
+### Lightweight-Chart preparation
+
+1. Run ```npm install lightweight-charts```
+2. Run ```cp node_modules/lightweight-charts/dist/lightweight-charts.standalone.production.js frontend/static/scripts/```
+
 ### Tailwind
 
 1. Run ```npx tailwindcss -i ./frontend/static/css/input.css -o ./frontend/static/css/output.css --watch```
--- frontend/static/scripts/index_script.js
+++ frontend/static/scripts/index_script.js
@@ -1,3 +1,37 @@
 document.getElementById("logout_button").addEventListener("click", function() {
             window.location.href = "/logout"
         });
+
+function initChart(containerId, data, candleData) {
+    const chartOptions = { layout: { textColor: 'black', background: { type: 'solid', color: 'white' } } };
+    const chart = createChart(document.getElementById(containerId), chartOptions);
+    // const chart = LightweightCharts.createChart(document.getElementById(containerId), {
+    //     width: 600,
+    //     height: 400,
+    //     layout: {
+    //         backgroundColor: '#ffffff',
+    //         textColor: '#000',
+    //     },
+    //     grid: {
+    //         vertLines: { color: '#eee' },
+    //         horzLines: { color: '#eee' },
+    //     },
+    // });
+    // const lineSeries = chart.addLineSeries();
+    // lineSeries.setData(data);
+
+    const areaSeries = chart.addAreaSeries({
+        lineColor: '#2962FF', topColor: '#2962FF',
+        bottomColor: 'rgba(41, 98, 255, 0.28)',
+    });
+
+    areaSeries.setData(data);
+    const candlestickSeries = chart.addCandlestickSeries({
+        upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+        wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+    });
+
+    candlestickSeries.setData(candleData);
+
+    chart.timeScale().fitContent();
+}
